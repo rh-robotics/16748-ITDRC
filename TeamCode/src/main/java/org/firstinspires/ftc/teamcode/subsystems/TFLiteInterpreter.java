49e67947f317791e051236c9d5018382e3ccba42
aurora-cichos <auroracichos7@gmail.com>
2024-11-22 14:57:49 -0700

    Added litert-support to TeamCode dependencies, the tflite model and labels to TeamCode assets, and an interpreter class for interfacing with the tflite model

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TFLiteInterpreter.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import android.content.Context;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.util.List;
+import java.util.Map;
+
+import org.tensorflow.lite.InterpreterApi;
+public class TFLiteInterpreter {
+    public static final String ASSOCIATED_AXIS_LABELS = "labels.txt";
+    private TensorBuffer probabilityBuffer;
+    private InterpreterApi tflite;
+    private List<String> associatedAxisLabels;
+
+    public TFLiteInterpreter() {
+        probabilityBuffer = TensorBuffer.createFixedSize(
+                new int[]{1, 1001}, DataType.UINT8);
+        tflite = null;
+        associatedAxisLabels = null;
+    }
+    public TFLiteInterpreter(int n) {
+        probabilityBuffer = TensorBuffer.createFixedSize(
+                new int[]{1, n}, DataType.UINT8);
+        tflite = null;
+        associatedAxisLabels = null;
+    }
+
+    IOException loadModel(String file, Context context) {
+        try {
+            MappedByteBuffer tfliteModel = FileUtil.loadMappedFile(context, file);
+            InterpreterApi tflite = InterpreterApi.create(
+                    tfliteModel, new InterpreterApi.Options());
+        } catch(IOException e) {
+            return e;
+        }
+
+        return null;
+    }
+
+    IOException loadLabels(Context context) {
+        try {
+            associatedAxisLabels = FileUtil.loadLabels(context, ASSOCIATED_AXIS_LABELS);
+        } catch (IOException e) {
+            return e;
+        }
+
+        return null;
+    }
+
+    int runModel(MappedByteBuffer img) {
+        if (tflite == null) {
+            return 1;
+        }
+
+        tflite.run(img, probabilityBuffer.getBuffer());
+        return 0;
+    }
+
+    Map<String, Float> process() {
+        TensorProcessor probabilityProcessor =
+                new TensorProcessor.Builder().add(new NormalizeOp(0, 255)).build();
+
+        if (associatedAxisLabels != null) {
+            TensorLabel labels = new TensorLabel(associatedAxisLabels,
+                    probabilityProcessor.process(probabilityBuffer));
+
+            return labels.getMapWithFloatValue();
+        }
+
+        return null;
+    }
+
+}
